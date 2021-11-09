529354d430d217cd3a9f94249c5df916233bde6f
Nuwan Dewapriya <67752825+nuwan-d@users.noreply.github.com>
2021-11-09 12:28:48 -0500

    Add files via upload

---
+++ VASP_Polyurethane/submit_to_Niagara.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --account=YOUR_ACCOUNT
+#SBATCH --nodes=20
+#SBATCH --ntasks-per-node=40
+#SBATCH --time=23:45:00
+#SBATCH --mail-user=YOUR_EMAIL
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=Comp_0.5_Temp_2800
+
+# Information about Niagara is available here: https://docs.computecanada.ca/wiki/Niagara
+
+module load NiaEnv/2019b
+module load intel/2020u4
+module load intelmpi/2020u4
+module use /scinet/niagara/software/commercial/modules
+module load vasp/6.1.2
+
+cd $SLURM_SUBMIT_DIR
+
+srun vasp_std
+
